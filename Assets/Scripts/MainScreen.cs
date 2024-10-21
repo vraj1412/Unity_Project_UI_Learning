b9c1806408d64e8e693434f623aabf312d6b1dbe
vraj1412 <sq.vraj1410@gmail.com>
2024-10-21 07:21:16 +0530

    update

---
+++ Assets/Scripts/MainScreen.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScreen : MonoBehaviour
+{
+    public void SessionPass()
+    {
+        Debug.Log("Session Pass Opened..");
+    }
+    public void heroes()
+    {
+        Debug.Log("Heroes Opened..");
+    }
+    public void eqipment()
+    {
+        Debug.Log("Eqipment Opened..");
+    }
+    public void artifact()
+    {
+        Debug.Log("Artifact Opened..");
+    }
+    public void upgrade()
+    {
+        Debug.Log("Upgrade Opened..");
+    }
+    public void shop()
+    {
+        Debug.Log("Shop Opened..");
+    }
+    public void world()
+    {
+        Debug.Log("World Opened..");
+    }
+    public void Start()
+    {
+        Debug.Log("Start Opened..");
+    }
+    public void mission()
+    {
+        Debug.Log("Mission Opened..");
+    }
+    public void club()
+    {
+        Debug.Log("Club Opened..");
+    }
+    public void ranking()
+    {
+        Debug.Log("Ranking Opened..");
+    }
+    public void setting()
+    {
+        Debug.Log("Setting Opened..");
+    }
+}
