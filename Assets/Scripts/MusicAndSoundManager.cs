10ae34043d80fd0feac84ea513cd141571786a84
vraj1412 <sq.vraj1410@gmail.com>
2024-12-05 08:16:47 +0530

    Sound And Music Script Assign

---
+++ Assets/Scripts/MusicAndSoundManager.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicAndSoundManager : MonoBehaviour
+{
+    public AudioSource SoundAudioSource;
+    public AudioSource MusicAudioSource;
+
+    public static MusicAndSoundManager instance;
+
+
+
+    #region Unity Function
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Start()
+    {
+        StartSetData();
+    }
+    #endregion
+
+    #region Get/Set Data 
+    public void StartSetData()
+    {
+        SetSound_Volume(1);
+        SetMusic_Volume(1);
+    }
+
+    private void SetSound_Volume(object sound)
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
+
+    #region Sound Function
+    public void PlaySound(AudioClip clip)
+    {
+        SoundAudioSource.clip = clip;
+        SoundAudioSource.Play();
+    }
+
+
+    public void SoundMute(bool mute)
+    {
+
+        SoundAudioSource.mute = mute;
+    }
+    public void SetSound_Volume(float Volume)
+    {
+        SoundAudioSource.volume = Volume;
+    }
+    #endregion
+
+    #region Music Function
+    public void PlayMusic(AudioClip clip)
+    {
+        MusicAudioSource.clip = clip;
+        MusicAudioSource.Play();
+    }
+
+    public void MuiscMute(bool mute)
+    {
+        MusicAudioSource.mute = mute;
+    }
+
+    public void SetMusic_Volume(float Volume)
+    {
+        MusicAudioSource.volume = Volume;
+    }
+    #endregion
+
+
+
+
+    #region Other Function
+    public void PauseSoundAndMusic(bool IsPuse)
+    {
+        if (IsPuse)
+        {
+            SoundAudioSource.Pause();
+            MusicAudioSource.Pause();
+        }
+        else
+        {
+            SoundAudioSource.UnPause();
+            MusicAudioSource.UnPause();
+        }
+    }
+    #endregion
+}
---
+++ Assets/UiManager.cs
@@ -0,0 +1,179 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+    [Header("Slider")]
+    public Slider SoundSlider;
+    public Slider MusicSlider;
+    [Space]
+    [Header("ImageParent")]
+    public Image SoundImage;
+    public Image MusicImage;
+    [Space]
+    [Header("ImageSprite")]
+    public Sprite SoundSprite;
+    public Sprite MusicSprite;
+    public Sprite MuteSoundSprite;
+    public Sprite MuteMusicSprite;
+    [Space]
+    [Header("Popup Refanace")]
+    public GameObject PopUp;
+    public GameObject PopUpBg;
+
+
+    public MusicAndSoundManager musicAndSoundManager;
+    //SoundAndMusic SoundMusic;
+
+    public void Start()
+    {
+        //  SoundSlider.value = StaticData.Sound;
+        //  MusicSlider.value = StaticData.Music;
+        //   SetSoundAndMusic();
+
+        //  SoundMusic = SoundAndMusic.instance;
+
+        musicAndSoundManager = MusicAndSoundManager.instance;
+
+    }
+
+
+    public void ButtonControl(string button)
+    {
+
+        switch (button)
+        {
+            case "Close":
+                //AnimationManager.Scale(PopUpBg.transform, 0.5f, EndPostion: Vector3.zero, StartPostion: Vector3.one, ease: Ease.InFlash, EndAction:
+                //    () => {
+                //        PopUpBg.transform.localScale = Vector3.one;
+                //        PopUp.SetActive(false);
+                //    }
+                //    );
+                //PopUp.SetActive(false);
+                break;
+            case "Sound":
+                Sound_Slidar();
+                break;
+            case "Music":
+                Music_Slidar();
+                break;
+            case "Sound_Mute":
+                Sound_Icon();
+                break;
+            case "Music_Mute":
+                Music_Icon();
+                break;
+        }
+    }
+    public void Sound_Slidar()
+    {
+      //  StaticData.Sound = SoundSlider.value;
+        //soundandmusic.SoundAudioSource.volume = SoundSlider.value;
+        //SoundAndMusic.instance.SetSound_Volume(SoundSlider.value);
+        //if (StaticData.Sound == 0)
+        //{
+        //    SoundMute(true);
+        //}
+        //else
+        //{
+        //    SoundMute(false);
+        //}
+    }
+
+    public void Music_Slidar()
+    {
+        //StaticData.Music = MusicSlider.value;
+        //SoundAndMusic.instance.SetMusic_Volume(MusicSlider.value);
+        //// soundandmusic.MusicAudioSource.volume = MusicSlider.value;
+        //if (StaticData.Music == 0)
+        //{
+        //    MusicMute(true);
+        //}
+        //else
+        //{
+        //    MusicMute(false);
+        //}
+    }
+
+    public void Music_Icon()
+    {
+        //if (StaticData.MuteMusic == 0)
+        //{
+        //    MusicMute(true);
+        //}
+        //else
+        //{
+        //    MusicMute(false);
+        //}
+    }
+
+    public void Sound_Icon()
+    {
+        //if (StaticData.MuteSound == 0)
+        //{
+        //    SoundMute(true);
+        //}
+        //else
+        //{
+        //    SoundMute(false);
+        //}
+
+    }
+
+    public void SoundMute(bool mute)
+    {
+
+        //if (mute)
+        //{
+        //    StaticData.MuteSound = 1;
+        //    SoundImage.sprite = MuteSoundSprite;
+        //    SoundAndMusic.instance.SoundMute(true);
+        //}
+        //else
+        //{
+        //    StaticData.MuteSound = 0;
+        //    SoundImage.sprite = SoundSprite;
+        //    SoundAndMusic.instance.SoundMute(false);
+        //}
+    }
+
+    public void MusicMute(bool mute)
+    {
+        //if (mute)
+        //{
+        //    StaticData.MuteMusic = 1;
+        //    MusicImage.sprite = MuteMusicSprite;
+        //    SoundAndMusic.instance.MuiscMute(true);
+        //}
+        //else
+        //{
+        //    StaticData.MuteMusic = 0;
+        //    MusicImage.sprite = MusicSprite;
+        //    SoundAndMusic.instance.MuiscMute(false);
+        //}
+    }
+
+    public void SetSoundAndMusic()
+    {
+    //    if (StaticData.MuteSound == 0)
+    //    {
+    //        SoundMute(false);
+    //    }
+    //    else
+    //    {
+    //        SoundMute(true);
+    //    }
+
+    //    if (StaticData.MuteMusic == 0)
+    //    {
+    //        MusicMute(false);
+    //    }
+    //    else
+    //    {
+    //        MusicMute(true);
+    //    }
+    }
+}
